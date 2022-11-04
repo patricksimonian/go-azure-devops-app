d4af2014cc624826db48ddd800a57aef8ba256d1
Patrick Simonian <patricksimonian@gmail.com>
2022-11-04 15:50:54 -0700

    add function app bits

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func httpTriggerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("X-Forwarded-For") != "" {
+		w.Write([]byte(strings.Split(r.Header.Get("X-Forwarded-For"), ":")[0]))
+	} else if r.Header.Get("Host") != "" {
+		w.Write([]byte(strings.Split(r.Header.Get("Host"), ":")[0]))
+	} else if r.RemoteAddr != "" {
+		w.Write([]byte(strings.Split(r.RemoteAddr, ":")[0]))
+	}
+}
 
 func main() {
 	fmt.Println("Hello world!")
+
+	httpInvokerPort, exists := os.LookupEnv("FUNCTIONS_HTTPWORKER_PORT")
+	if exists {
+		fmt.Println("FUNCTIONS_HTTPWORKER_PORT: " + httpInvokerPort)
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/foo", httpTriggerHandler)
+	log.Println("Go server Listening...on httpInvokerPort:", httpInvokerPort)
+	log.Fatal(http.ListenAndServe(":"+httpInvokerPort, mux))
 }
